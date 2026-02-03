2e6db84c3d11ad52e18cb5c84f0942af8c2a946f
Sagar Chandra <sagarchandra1824@gmail.com>
2026-02-03 20:01:19 +0530

    Add robust voice perception layer (Vosk)

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from voice.input import listen
+
 def main():
     print("Personal Cognitive OS booting...")
+    text = listen(seconds=10)
+    print("Heard:", text)
 
 if __name__ == "__main__":
     main()
